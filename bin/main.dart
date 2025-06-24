2b9bab8893abdbf4331723c02c82e372f408cbd9
newtox <futzy261@gmail.com>
2025-06-24 21:30:38 +0200

    Changed intents that are being used.

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
 import 'package:dotenv/dotenv.dart';
-import 'package:toxbot/events.dart';
 import 'package:nyxx/nyxx.dart';
 import 'package:nyxx_commands/nyxx_commands.dart';
+import 'package:toxbot/events.dart';
 
 void main() async {
   var env = DotEnv(includePlatformEnvironment: true)..load();
@@ -16,19 +16,18 @@ void main() async {
           defaultResponseLevel: ResponseLevel.public,
           type: CommandType.slashOnly));
 
-  final client =
-      await Nyxx.connectGateway(env['token']!, GatewayIntents.allUnprivileged,
-          options: GatewayClientOptions(plugins: [
-            Logging(
-                stderrLevel: Level.SEVERE,
-                stackTraceLevel: Level.SEVERE,
-                logLevel: Level.INFO,
-                censorToken: true,
-                stdout: stdout,
-                stderr: stderr),
-            cliIntegration,
-            setupCommandHandler(commands)
-          ]));
+  final client = await Nyxx.connectGateway(env['token']!, GatewayIntents.all,
+      options: GatewayClientOptions(plugins: [
+        Logging(
+            stderrLevel: Level.SEVERE,
+            stackTraceLevel: Level.SEVERE,
+            logLevel: Level.INFO,
+            censorToken: true,
+            stdout: stdout,
+            stderr: stderr),
+        cliIntegration,
+        setupCommandHandler(commands)
+      ]));
 
   setupReadyHandler(client);
   setupGuildCreateHandler(client);
